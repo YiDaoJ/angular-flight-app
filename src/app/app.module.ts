c65f96d6e7eb8dcd9e99d4808a352d2babda2795
YiDaoJ <jcxsusan@gmail.com>
2023-05-31 20:35:44 +0200

    add flight search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
 
 @NgModule({
-   imports: [
-      BrowserModule
-   ],
-   declarations: [
-      AppComponent,
-      SidebarComponent,
-      NavbarComponent
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    FlightSearchComponent,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
