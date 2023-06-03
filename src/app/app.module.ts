5420d3c963c3165e587a3a49e5333a8d7e90d26e
YiDaoJ <jcxsusan@gmail.com>
2023-06-03 12:06:46 +0200

    config routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,35 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
-import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { APP_ROUTES } from './app.routes';
 import { FlightCardComponent } from './flight-card/flight-card.component';
+import { FlightEditComponent } from './flight-edit/flight-edit.component';
+import { FlightSearchComponent } from './flight-search/flight-search.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PassengerSearchComponent } from './passenger-search/passenger-search.component';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(APP_ROUTES),
+  ],
   declarations: [
     AppComponent,
     SidebarComponent,
     NavbarComponent,
     FlightSearchComponent,
     FlightCardComponent,
+    HomeComponent,
+    PassengerSearchComponent,
+    FlightEditComponent,
+    AboutComponent,
+    NotFoundComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
